23a04925e811f1419d74efc7519affe4c83303c4
Simon Emms <simon@simonemms.com>
2017-01-15 15:19:39 +0000

    Configured the default project

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+echo "npm version:"
+npm --version
+
+cd build
+
 # Login to npm
-printf "%s\n%s\n%s" $NPM_USER $NPM_PASSWORD $NPM_EMAIL | npm login --scope=@steeplejack && npm publish --access public
+printf "%s\n%s\n%s" $NPM_USER $NPM_PASSWORD $NPM_EMAIL | npm login --scope=@steeplejack && npm publish --access public || :
