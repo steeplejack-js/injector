71a088e23bb6c6e9a211c3a2f54c9de558065560
Simon Emms <simon@simonemms.com>
2017-02-05 21:03:38 +0000

    Wrote the unit tests for the function version of the processor

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -3,7 +3,6 @@
  */
 
 /* Node modules */
-import { EventEmitter } from 'events';
 
 /* Third-party modules */
 import { Base } from '@steeplejack/core';
@@ -14,17 +13,198 @@ import Injector from '../../src/index';
 
 describe('Injector library', function () {
 
-  describe('Instantiation', function () {
+  beforeEach(function () {
 
-    it('should be the correct instances', function () {
+    const obj = new Injector();
 
-      const obj = new Injector();
+    expect(obj).to.be.instanceof(Injector)
+      .to.be.instanceof(Base);
 
-      expect(obj).to.be.instanceof(Injector)
-        .to.be.instanceof(Base)
-        .to.be.instanceof(EventEmitter);
+    expect(obj.components).to.be.eql({});
+    expect(obj.getComponent('non-existent')).to.be.undefined;
 
-      expect(obj.getComponent('non-existent')).to.be.null;
+  });
+
+  describe('methods', function () {
+
+    describe('#process', function () {
+
+      beforeEach(function () {
+        this.obj = new Injector();
+
+        /* Set some components */
+        this.obj
+          .registerComponent({
+            factory: () => ({
+              hello: 'factoryNoDeps',
+            }),
+            name: 'factoryNoDeps',
+            path: '/path/to/fnd',
+          })
+          .registerComponent({
+            deps: [
+              'instancePath',
+            ],
+            factory: dep1 => ({
+              dep1,
+            }),
+            name: 'factoryDep',
+          })
+          .registerComponent({
+            instance: 'instanceNoPath',
+            name: 'instanceNoPath',
+          })
+          .registerComponent({
+            instance: 'instancePath',
+            name: 'instancePath',
+            path: '/path/to/ip',
+          })
+          .registerComponent({
+            instance: () => 'instanceNoPathFactory',
+            name: 'instanceNoPathFactory',
+          })
+          .registerComponent({
+            instance: () => 'instancePathFactory',
+            name: 'instancePathFactory',
+            path: '/path/to/ipf',
+          })
+          .registerComponent({
+            instance: () => { throw new Error('ignored'); },
+            name: 'ignored',
+          });
+      });
+
+      describe('dependencies gotten from the function', function () {
+
+        describe('function', function () {
+
+          it('should process a target that has no dependencies', function () {
+
+            const target = function () {};
+
+            expect(this.obj.process(target)).to.be.instanceof(target);
+
+          });
+
+          it('should process a target that has a top-level dependency', function () {
+
+            const target = function (topLevel) {
+              this.exec = topLevel;
+            };
+
+            const objTarget = this.obj.process(target, [
+              'instancePathFactory',
+            ]);
+            expect(objTarget).to.be.instanceof(target);
+
+            expect(objTarget.exec()).to.be.equal('instancePathFactory');
+
+          });
+
+          it('should process a target that has a multiple top-level dependencies', function () {
+
+            const target = function (topLevel, otherFunc) {
+
+              this.exec = topLevel;
+              this.otherFunc = otherFunc;
+
+            };
+
+            const objTarget = this.obj.process(target, [
+              'instanceNoPathFactory',
+              'instancePathFactory',
+            ]);
+            expect(objTarget).to.be.instanceof(target);
+            expect(objTarget.exec()).to.be.equal('instanceNoPathFactory');
+            expect(objTarget.otherFunc()).to.be.equal('instancePathFactory');
+
+          });
+
+          it('should get a constructor dependency', function () {
+
+            const target = function (topLevel) {
+
+              this.exec = topLevel;
+
+            };
+
+            const objTarget = this.obj.process(target, [
+              'factoryNoDeps',
+            ]);
+
+            expect(objTarget.exec).to.be.eql({
+              hello: 'factoryNoDeps',
+            });
+
+          });
+
+          it('should process stacked dependencies', function () {
+
+            const target = function (topLevel) {
+
+              this.exec = topLevel;
+
+            };
+
+            const objTarget = this.obj.process(target, [
+              'factoryDep',
+            ]);
+
+            expect(objTarget.exec).to.be.eql({
+              dep1: 'instancePath',
+            });
+
+          });
+
+          it('should throw an error if dependency is missing', function () {
+
+            const target = function (topLevel, otherFunc) {
+
+              this.exec = topLevel;
+              this.otherFunc = otherFunc;
+
+            };
+
+            let fail = false;
+            try {
+              this.obj.process(target, [
+                'instancePathFactory',
+                'otherFunc',
+              ]);
+            } catch (err) {
+              fail = true;
+
+              expect(err).to.be.instanceof(Error);
+              expect(err.message).to.be.equal('Missing dependency: otherFunc');
+            }
+
+            expect(fail).to.be.true;
+
+          });
+
+          it('should throw an error if too many arguments compared to injected dependencies - zero', function () {
+
+            const target = function (someDep) {
+              this.exec = someDep;
+            };
+
+            let fail = false;
+            try {
+              this.obj.process(target);
+            } catch (err) {
+              fail = true;
+
+              expect(err).to.be.instanceof(Error);
+              expect(err.message).to.be.equal(`Missing argument in argument 1: ${target.toString()}`);
+            } finally {
+              expect(fail).to.be.true;
+            }
+
+          });
+
+        });
+
+      });
 
     });
 
