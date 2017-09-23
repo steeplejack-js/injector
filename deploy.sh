211b9ba8c84b81041a622ab4625031c3d9d78191
Simon Emms <simon@simonemms.com>
2017-09-23 16:44:44 +0100

    Updated deployment for npm

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "npm version:"
-npm --version
-
-cd build
-
-# Login to npm
-printf "%s\n%s\n%s" $NPM_USER $NPM_PASSWORD $NPM_EMAIL | npm login --scope=@steeplejack && npm publish --access public || :
